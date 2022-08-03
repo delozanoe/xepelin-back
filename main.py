d10cb361cecf7999f2b54c76f8249079bd2a36fb
Daniel Lozano <daniellozano.ee@gmail.com>
2022-08-03 01:41:11 -0500

    DL: app final commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+from fastapi import FastAPI
+import http.client
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.post('/send-email/')
+def send_email(idOp, tasa, email):
+    conn = http.client.HTTPSConnection("hooks.zapier.com")
+    payload = json.dumps({
+        "idOp": idOp,
+        "tasa": tasa,
+        "email": email
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/hooks/catch/6872019/oahrt5g/", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+    return {"menssage": data.decode("utf-8")}
+
