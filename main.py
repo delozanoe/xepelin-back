2cb30a4a88a196cc95d91ef70792ecf320f44a4d
Daniel Lozano <daniellozano.ee@gmail.com>
2022-08-03 02:19:32 -0500

    YAAAAA

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+app = FastAPI()
 
 
 @app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+async def root():
+    return {"message": "Hello World"}
+
