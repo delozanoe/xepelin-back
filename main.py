a7af2705a2dcdda8002b5cc94856d9a6be496b2f
Daniel Lozano <daniellozano.ee@gmail.com>
2022-08-03 02:49:51 -0500

    DL: send email

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+import http.client
+import json
 app = FastAPI()
 
 
@@ -11,3 +12,22 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post('/send-email/')
+def send_email(idOp, tasa, email):
+    conn = http.client.HTTPSConnection("hooks.zapier.com")
+    payload = json.dumps({
+        "idOp": idOp,
+        "tasa": tasa,
+        "email": email
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/hooks/catch/6872019/oahrt5g/", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+    return {"menssage": data.decode("utf-8")}
