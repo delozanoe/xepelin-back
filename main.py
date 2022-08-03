21235260106af0c8c4b3fd0c591a82a2039941e4
Daniel Lozano <daniellozano.ee@gmail.com>
2022-08-03 02:39:38 -0500

    DL: prueba proyecto virgen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
@@ -8,3 +7,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
